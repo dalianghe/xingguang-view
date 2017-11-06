f6dae1b633373a2b525dfd2c93cccc9b1056afda
zongxu <15116951695@163.com>
2017-11-07 00:21:16 +0800

    调整图片位置

--- xg_cus_view/src/components/bankcard/addbank.vue
+++ xg_cus_view/src/components/bankcard/addbank.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="box">
     <mt-popup
-      class="mint-popup-4"
+      class="mint-popup-width-full"
       v-model="popupVisible"
       popup-transition="popup-fade"
       position="bottom">
-      <mt-picker :slots="slots" @change="selectedBank"></mt-picker>
+      <mt-picker :slots="slots" valueKey="name" @change="selectedBank"></mt-picker>
     </mt-popup>
     <div class="content clears">
-
       <div class="input">
         <dl>
-          <dt @click="selectBank">银行卡</dt>
-          <dd>
-            <select v-model="data.bankId">
-              <option v-for="obj in options" :value="obj.id">{{obj.name}}</option>
-            </select>
+          <dt>银行卡</dt>
+          <dd @click="selectBank">
+            <input type="text" class="text" readonly="readonly" v-model="data.bankName"/>
           </dd>
         </dl>
         <dl>
@@ -51,18 +48,8 @@
         slots: [
           {
             flex: 1,
-            values: ['1','2','3'],
-            className: 'slot1',
-            textAlign: 'right'
-          }, {
-            divider: true,
-            content: '-',
-            className: 'slot2'
-          }, {
-            flex: 1,
-            values: ['a','b','c'],
-            className: 'slot3',
-            textAlign: 'left'
+            values: [],
+            className: 'slot1'
           }
         ]
       };
@@ -74,14 +61,21 @@
       selectBank : function(){
         this.popupVisible = true;
       },
-      selectedBank : function(){
-
+      selectedBank : function(picker, values){
+        if(!this.popupVisible){
+          return;
+        }
+        if(picker.getValues()[0]){
+          this.data.bankId = picker.getValues()[0].id;
+          this.data.bankName = picker.getValues()[0].name;
+        }
       },
       getBanks: function (event) {
         var vm = this;
         this.$http.get('/banks')
           .then(function (response) {
             if (response.bizCode == 0) {
+              vm.slots[0].values = response.data;
               vm.options = response.data;
             }
           })
@@ -91,6 +85,7 @@
 
       submit: function (event) {
         var vm = this;
+        console.info(vm.$data.data.bankId);
         vm.$indicator.open();
         this.$http.post('/bank/cards', vm.$data.data)
           .then(function (response) {
@@ -110,86 +105,5 @@
 </script>
 
 <style scoped>
-  .box{
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    right: 0px;
-    bottom: 0px;
-    background-color: #F8F8F8;
-  }
-
-  .box .content{
-    vertical-align: top;
-    background-color: #FFF;
-  }
-  .box .content .input{
-  }
-
-  .box .content .input .text{
-    border: none;
-    height: 3.5rem;
-  }
-
-  dl, dt, dd {
-    display: block;
-  }
-
-  dl {
-    overflow: hidden;
-    width: 92%;
-    margin: 0 auto;
-    height: 3.5rem;
-    border-bottom: 1px solid rgba(230, 233, 237, 1);
-  }
-
-  dt {
-    width: 30%;
-    float: left;
-    color: #999999;
-    line-height: 3.5rem;
-  }
-
-  dd {
-    margin: 0;
-    padding: 0;
-    float: left;
-    color: #333333;
-    line-height: 3.5rem;
-  }
-
-  .btns{
-    margin-top: 1.5rem;
-  }
-
-  .btnbox{
-    width: 94%;
-    margin: 0 auto;
-  }
-
-  .btnbox a{
-    display: block;
-    color: #ffffff;
-    background-color: rgba(241, 2, 22, 1);
-    border-radius: 5px 5px 5px 5px;
-    height: 3rem;
-    text-align: center;
-    line-height: 3rem;
-    font-size: 1.2rem;
-  }
-
-  .selectbox{
-
-  }
-  .selectbox ul{
-    list-style: none outside none;
-  }
-  .selectbox ul li{
-    text-align: center;
-    border-bottom: 1px solid #eee;
-    display: list-item;
-    height: 3rem;
-    line-height: 3rem;
-  }
 
 </style>
