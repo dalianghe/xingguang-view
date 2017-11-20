8dc71b39f62189ecdb3f325a2e20eb8881bd4c78
zongxu <15116951695@163.com>
2017-11-20 20:26:10 +0800

    添加银行卡默认

--- xg_cus_view/src/components/bankcard/bank_list.vue
+++ xg_cus_view/src/components/bankcard/bank_list.vue
@@ -6,13 +6,13 @@
         <img class="img" src="/static/cus/img/add.png"/>
       </router-link>
     </div>
-    <div v-show="bankList.length == 0" class="contentAlert">
+    <div v-show="list.length == 0" class="contentAlert">
       <div class="img unlist"></div>
       <div class="msg">暂无数据</div>
     </div>
     <div class="bank_box">
       <transition-group name="root" tag="p" mode="out-in">
-        <div class="bank clears" v-for="obj in bankList" v-show="obj.cusBankCard.status == 1" key="obj.cusBankCard.id">
+        <div :class="{ bank: true, clears: true, def: obj.cusBankCard.isDefault == 1 }" v-for="obj in list" v-show="obj.cusBankCard.status == 1" key="obj.cusBankCard.id">
           <div class="icon_box">
             <img class="icon" :src="'/static/cus/img/bank/'+ obj.cusBank.path + '.png'" />
           </div>
@@ -25,6 +25,7 @@
             <div class="bank_no">**** **** **** {{ obj.cusBankCard.cardNo }}</div>
           </div>
           <div class="unbind">
+            <a class="list_button" href="javascript:;" v-if="obj.cusBankCard.isDefault == 2" @click="def(obj.cusBankCard.id)">设为默认</a>
             <a class="list_button" href="javascript:;" @click="unbind(obj.cusBankCard.id)">解绑</a>
           </div>
         </div>
@@ -39,7 +40,7 @@
     name: 'banklist',
     data() {
       return {
-        bankList: []
+        list: []
       }
     },
     mounted: function () {
@@ -50,7 +51,7 @@
         var vm = this;
         this.$http.get('/bank/cards')
           .then(function (response) {
-            vm.bankList = response.data;
+            vm.list = response.data;
           })
       },
       unbind: function (id) {
@@ -59,7 +60,7 @@
           this.$http.delete('/bank/cards/' + id)
             .then(function (response) {
               if (response.bizCode == 0) {
-                for (var bank of vm.bankList) {
+                for (var bank of vm.list) {
                   if (bank.id == id) {
                     bank.cusBankCard.status = 2;
                     break;
@@ -74,8 +75,33 @@
 
         });
       },
-      showMsgbox: function () {
+      def: function (id) {
+        var vm = this;
+        vm.$messagebox.confirm('确定要设为默认账户吗?').then(action => {
+          this.$http.patch('/bank/cards/' + id + '/def')
+            .then(function (response) {
+              if (response.bizCode == 0) {
+                let i = 0;
+                let k = 0;
+                for (var bank of vm.list) {
+                  if (bank.id == id) {
+                    k = i;
+                    bank.cusBankCard.isDefault = 1;
+                  }else{
+                    bank.cusBankCard.isDefault = 2;
+                    i++;
+                  }
+                }
+                vm.list.splice(k, 1);
+                vm.list.unshift(bank);
+                vm.$toast("设置成功!");
+              } else {
+                vm.$toast("设置失败!");
+              }
+            })
+        }).catch(err => {
 
+        });
       }
     }
   }
@@ -91,7 +117,13 @@
     background: #FFFFFF;
     margin-bottom: 1rem;
     border-bottom: 1px solid rgba(230, 233, 237, 1);
+    border-top: 1px solid rgba(230, 233, 237, 1);
     box-shadow: 1px 1px 2px rgba(230, 233, 237, 1);
+  }
+
+  .box .def{
+    background:rgba(255,255,255,5) url(/static/cus/img/def.png) no-repeat center center;
+    background-size: 4rem 4rem;
   }
 
   .box .top {
@@ -127,6 +159,7 @@
   .box .bank_box .detail {
     float: left;
     margin: 0.5rem;
+    background-color: rgba(255, 255, 255, 0.6);
   }
 
   .box .bank_box .detail .title {
@@ -140,7 +173,7 @@
     margin-left: 1rem;
     display: inline-block;
     font-size: 0.8rem;
-    color: #999999;
+    color: #666666;
     line-height: 1.6rem;
   }
 
@@ -148,22 +181,23 @@
     margin-left: 0.2rem;
     display: inline-block;
     font-size: 0.8rem;
-    color: #999;
+    color: #666666;
     line-height: 1.6rem;
   }
 
   .box .bank_box .detail .bank_no {
     margin-left: 1rem;
     font-size: 1rem;
-    color: #666;
+    color: #666666;
     line-height: 1.6rem;
     letter-spacing: 0.15rem;
   }
 
   .box .bank_box .unbind {
     float: right;
-    padding-top: 2rem;
+    padding-top: 1rem;
     padding-right: 1rem;
+    width: 5.5rem;
   }
 
   .list_button {
@@ -171,13 +205,15 @@
     outline: none;
     font-size: 0.8rem;
     color: #a43b3b;
-    padding: 0.3rem 1rem;
+    padding: 0.3rem 0.3rem;
     border-radius: 0.2rem;
-    display: inline-block;
+    display: block;
     background-clip: padding-box;
     transition-duratio: 0.2s;
     background-color: #f6f8fa;
     box-shadow: 0 0 1px #b8bbbf;
+    text-align: center;
+    margin-bottom: 0.5rem;
   }
 
 </style>
